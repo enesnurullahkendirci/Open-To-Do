051a40f3ad859a6457bf141b3bd01524d3ea6d73
Enes Nurullah Kendirci <enesnkendirci@gmail.com>
2021-10-13 11:43:05 +0300

    detail bug fix

--- todo/CoreDataManager.swift
+++ todo/CoreDataManager.swift
@@ -102,11 +102,12 @@ class CoreDataManager: CoreDataManagerProtocol {
     private func createToDoFromNSManagedObject(managedObject item: NSManagedObject) -> ToDo{
         let id = item.value(forKey: ToDoItemEnum.id.rawValue) as! Int
         let title = item.value(forKey: ToDoItemEnum.title.rawValue) as! String
+        let detail = item.value(forKey: ToDoItemEnum.detail.rawValue) as? String
         let startDate = item.value(forKey: ToDoItemEnum.startDate.rawValue) as! Date
         let endDate = item.value(forKey: ToDoItemEnum.endDate.rawValue) as? Date
         let completed = item.value(forKey: ToDoItemEnum.completed.rawValue) as! Bool
         let color = item.value(forKey: ToDoItemEnum.color.rawValue) as! UIColor
-        let toDo = ToDo(id: id, title: title, startDate: startDate, endDate: endDate, completed: completed, color: color)
+        let toDo = ToDo(id: id, title: title, detail: detail, startDate: startDate, endDate: endDate, completed: completed, color: color)
         return toDo
     }
     
